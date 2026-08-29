1924c861b0835e7b2e4a0ffa4cc5d686728a1204
JssSalas <jcsalasg2901@yahoo.com>
2026-08-29 19:47:13 +0000

    chore: crea estructura inicial para preparación de datos

---
+++ src/evaluation/evaluate_policies.py
@@ -0,0 +1,41 @@
+
+import numpy as np
+
+def evaluar_politica(env, politica, semillas):
+    resultados = []
+
+    for semilla in semillas:
+        obs, info = env.reset(seed=semilla)
+
+        recompensa_total = 0.0
+        pasos = 0
+        eventos_adversos = 0
+        excesos_capacidad = 0
+        contactos = 0
+
+        while True:
+            accion = politica(env)
+            obs, reward, terminated, truncated, info = env.step(accion)
+
+            recompensa_total += float(reward)
+            pasos += 1
+
+            if accion > 0:
+                contactos += 1
+
+            eventos_adversos += int(info["evento_adverso"])
+            excesos_capacidad += int(info["excede_capacidad"])
+
+            if terminated or truncated:
+                break
+
+        resultados.append({
+            "semilla": semilla,
+            "recompensa_acumulada": recompensa_total,
+            "pasos": pasos,
+            "contactos": contactos,
+            "eventos_adversos": eventos_adversos,
+            "excesos_capacidad": excesos_capacidad
+        })
+
+    return resultados
